0619d4c08784deb217f95e09b19292c3ceb7eecf
PopanCristian <p_cristi10@yahoo.com>
2025-01-23 19:52:34 +0200

    First BluePrint

--- run.py
+++ run.py
@@ -1,10 +1,6 @@
 from app import create_app
 
-app=create_app()
-
-@app.route('/hello/world/diana')
-def home():
-    return "Salutare tuturor!"
+app = create_app()
 
 if __name__ == "__main__":
     app.run(debug=True)
