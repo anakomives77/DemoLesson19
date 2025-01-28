526b7cab9668688269a1ce1710e855985bc85659
anakomives77 <anakomives@yahoo.com>
2025-01-28 06:48:48 +0100

    ana

--- app/items/routes.py
+++ app/items/routes.py
@@ -8,3 +8,39 @@ def items_offering():
 @items.route('/show_layout')
 def layout_offering():
     return render_template("show_layout.html")
+
+
+@items.route('/events_list')
+def events_list_display():
+    list_of_events = [
+        {
+            "name": "cina cu Elena",
+            "date": "2025-01-21",
+            "time": "11:50",
+            "description": "in Bucuresti, la restaurant Vatra"
+        },
+    {
+        "name": "pranz cu Andrei",
+        "date": "2025-01-25",
+        "time": "13:30",
+        "description": "la restaurant Casa Verona"
+    },
+    {
+        "name": "intalnire de afaceri",
+        "date": "2025-01-30",
+        "time": "10:00",
+        "description": "in Bucuresti, biroul principal"
+    }
+    ]
+
+    return render_template("events_list.html",  events=list_of_events)
+
+@items.route('/event')
+def event_display():
+    events = {
+    "name": "intalnire de afaceri",
+    "date": "2025-01-30",
+    "time": "10:00",
+    "description": "in Bucuresti, biroul principal"}
+
+    return render_template("one_event.html", events=events)
