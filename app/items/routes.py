54095c3c1dcd81ea6e0fab870cb58527a04afd80
anakomives77 <anakomives@yahoo.com>
2025-01-30 08:56:32 +0100

    ana_assigment_30-01

--- app/items/routes.py
+++ app/items/routes.py
@@ -1,13 +1,13 @@
 from app.items import items
 from flask import render_template
 
-@items.route('/show_items_many_xxx2')
+@items.route('/layout_demo')
 def items_offering():
     return render_template("show_items.html")
 
 @items.route('/show_layout')
 def layout_offering():
-    return render_template("show_layout.html")
+    return render_template("show_items.html")
 
 
 
--- app/main/routes.py
+++ app/main/routes.py
@@ -8,6 +8,25 @@ def home():
 """
 
 @main.route('/')
+def main_page():
+    return render_template("main_view.html")
+
+@main.route('/add_event')
+def add_event():
+    return render_template("add_event.html")
+
+
+@main.route('/delete_event')
+def delete_event():
+    return render_template("delete_event.html")
+
+
+@main.route('/show_event')
+def show_event():
+    return render_template("show_event.html")
+
+
+@main.route('/events')
 def events_list_display():
     list_of_events = [
         {
